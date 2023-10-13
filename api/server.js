9b9f20a19e95fcb2c8d8bea48c4baa17ce6a8b4d
Fikriyah Ahmed <fikriyahahmed@Fikriyahs-MacBook-Pro.local>
2023-10-13 01:07:54 -0400

    first commit

--- api/server.js
+++ api/server.js
@@ -1,19 +1,19 @@
 
 import express from "express"
 
-import  OwnerRouter from "./owner"
-import  authorsRouter  from  "./authors"
-import  booksRouter   from "./books"
-import  bookstoresRouter from "./bookstores"
+// import  OwnerRouter from "./owner"
+import  authorsRouter  from  "./authors.js"
+import  booksRouter   from "./books.js"
+import  bookstoresRouter from "./bookstores.js"
 
 
 
 const server = express()
 
-server.use(express.jason())
+server.use(express.json())
 
 
-server.use("/api/owner",  OwnerRouter)
+// server.use("/api/owner",  OwnerRouter)
 server.use("/api/authors", authorsRouter)
 server.use("/api/books", booksRouter)
 server.use("/api/bookstores", bookstoresRouter)
